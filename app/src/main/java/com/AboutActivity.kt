4e0bcc20a954654805cfcd9fb7aed61e7c56c3a4
pk-focus <orinamitchel10@gmail.com>
2024-03-21 10:42:53 +0300

    today

--- app/src/main/java/com/AboutActivity.kt
+++ app/src/main/java/com/AboutActivity.kt
@@ -1,16 +1,32 @@
 package com
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.Madul.app.MainActivity
 import com.ui.theme.AppTheme
 
 class AboutActivity : ComponentActivity() {
@@ -28,10 +44,38 @@ class AboutActivity : ComponentActivity() {
 @Composable
 fun About() {
 
-    Column(){
+    Column(
+        modifier = Modifier
+            .background(Color.Green)
+            .fillMaxSize()
+            .padding(10.dp)
 
-        Text(text = "About")
-    }
-}
+    ) {
+
+        Text(text = "About Screen")
+        val home = LocalContext.current
+        Button(onClick = {
+            home.startActivity(Intent(home, MainActivity::class.java))
+
+        },
+            border = BorderStroke(2.dp, Color.Black),
+            colors = ButtonDefaults.outlinedButtonColors(Color.Cyan),
+            //colors = ButtonDefaults.buttonColors(Color.Black),
+            //shape = RectangleShape
+            //shape = RoundedCornerShape(10.dp),
+            shape = CutCornerShape(10)
+            ) {
+
+            Text(text = "Home Screen",color=Color.White)
+        }
+        val jj= AnnotatedString("you want to go Home? Click Here!!")
+
+        val ll = LocalContext.current
+        ClickableText(text =jj , onClick = {})
+
+        ll.startActivity(Intent(ll,MainActivity::class.java))
+
+
+    }}
 
 
--- app/src/main/java/com/Madul/app/MainActivity.kt
+++ app/src/main/java/com/Madul/app/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.Madul.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -11,6 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,10 +26,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.AboutActivity
 import com.Madul.app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,7 +62,9 @@ fun Greeting() {
 
         item {
             Row(
-                Modifier.fillMaxSize().background(color = Color.Cyan),
+                Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Cyan),
                 horizontalArrangement = Arrangement.SpaceEvenly,
 //
             ){
@@ -73,7 +87,8 @@ fun Greeting() {
 
 
             Text(text = "This is my first ever Android project", color = Color.Red, fontSize = 13.sp, fontFamily = FontFamily.Monospace)
-
+           
+            
             Text(text = "This is my home page", color = Color.Blue, fontSize = 15.sp, fontFamily = FontFamily.Serif, modifier = Modifier.background(
                 Color.Gray
             ))
@@ -82,10 +97,28 @@ fun Greeting() {
 
             Text(text = "Hello", color = Color.Black, fontFamily = FontFamily.Cursive)
 
+            val about = LocalContext.current
+            Button(onClick = {
+                about.startActivity(Intent(about,AboutActivity::class.java))
 
+            },
+                border = BorderStroke(2.dp, Color.Black),
+                colors = ButtonDefaults.outlinedButtonColors(Color.Cyan),
+//            colors = ButtonDefaults.buttonColors( Color.Cyan),
+                //shape = RectangleShape
+                shape = RoundedCornerShape(10.dp)
+//             shape = CutCornerShape(10)
+
+            ) {
+
+                Text(text = "About Screen",color=Color.Black)
+
+            }
+
+
+
+        }
 
         }
     }
 
-
-}
