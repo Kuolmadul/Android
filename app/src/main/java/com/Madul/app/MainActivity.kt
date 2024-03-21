c7d212f235eca2f82ace32e630e9c3c8252b4016
pk-focus <orinamitchel10@gmail.com>
2024-03-21 07:41:24 +0300

    h

--- app/src/main/java/com/Madul/app/MainActivity.kt
+++ app/src/main/java/com/Madul/app/MainActivity.kt
@@ -3,44 +3,89 @@ package com.Madul.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.Madul.app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Greeting() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
+    LazyColumn(
+        modifier = Modifier
+            .background(Color.Gray)
+//          .fillMaxWidth()
+//          .fillMaxHeight()
+            .fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+
+    ) {
+
+        item {
+            Row(
+                Modifier.fillMaxSize().background(color = Color.Cyan),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+//
+            ){
+
+                Text(text = "Chats")
+                Spacer(modifier = Modifier.width(45.dp))
+
+                Text(text = "Status")
+                Spacer(modifier = Modifier.width(45.dp))
+
+                Text(text = "Calls")
+                Spacer(modifier = Modifier.width(45.dp))
+
+                Text(text = "Community")
+                Spacer(modifier = Modifier.width(45.dp))
+
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+
+
+
+            Text(text = "This is my first ever Android project", color = Color.Red, fontSize = 13.sp, fontFamily = FontFamily.Monospace)
+
+            Text(text = "This is my home page", color = Color.Blue, fontSize = 15.sp, fontFamily = FontFamily.Serif, modifier = Modifier.background(
+                Color.Gray
+            ))
+
+            Text(text = "Hello",color = Color.Magenta, fontFamily = FontFamily.Cursive,fontSize = 19.sp)
+
+            Text(text = "Hello", color = Color.Black, fontFamily = FontFamily.Cursive)
+
+
+
+        }
     }
+
+
 }
--- app/src/main/java/com/madul/kuol/MainActivity.kt
+++ app/src/main/java/com/madul/kuol/MainActivity.kt
@@ -3,15 +3,29 @@ package com.madul.kuol
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.CacheDrawModifierNode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.madul.kuol.ui.theme.KuolTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,18 +42,58 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting() {
 
-    Column() {
+    LazyColumn(
+      modifier = Modifier
+          .background(Color.Gray)
+//          .fillMaxWidth()
+//          .fillMaxHeight()
+          .fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
 
-        Text(text = "This is my first ever Android project", color = Color.Red)
 
-        Text(text = "This is my home page", color = Color.Blue)
+    ) {
 
-        Text(text = "Hello", color = Color.Magenta)
+    item {
+        Row(
+            Modifier.fillMaxSize().background(color = Color.Cyan),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+//
+        ){
 
-        Text(text = "Hello", color = Color.Gray)
+            Text(text = "Chats")
+            Spacer(modifier = Modifier.width(45.dp))
+
+            Text(text = "Status")
+            Spacer(modifier = Modifier.width(45.dp))
+
+            Text(text = "Calls")
+            Spacer(modifier = Modifier.width(45.dp))
+
+            Text(text = "Community")
+            Spacer(modifier = Modifier.width(45.dp))
+
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+
+
+
+        Text(text = "This is my first ever Android project", color = Color.Red, fontSize = 13.sp, fontFamily = FontFamily.Monospace)
+
+        Text(text = "This is my home page", color = Color.Blue, fontSize = 15.sp, fontFamily = FontFamily.Serif, modifier = Modifier.background(
+            Color.Gray
+        ))
+
+        Text(text = "Hello",color = Color.Magenta, fontFamily = FontFamily.Cursive,fontSize = 19.sp)
+
+        Text(text = "Hello", color = Color.Black, fontFamily = FontFamily.Cursive)
+
+
 
     }
     }
 
+
+}
 
 
